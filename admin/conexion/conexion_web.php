7616d925d53427d486e68167a80a664e2706e1b6
wilmer contreras <wraulcontreras@gmail.com>
2018-03-01 17:53:23 -0500

    solo cambios visuales de codigo

---
+++ admin/conexion/conexion_web.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "global.php";
+
+$conexion = new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
+
+mysqli_query($conexion, 'SET NAMES "'.DB_ENCODE.'"');
+
+//Si tenemos un posible error en la conexion lo mostramos
+if (mysqli_connect_errno())
+{
+    printf("Fallo la conexion a la base de datos: %s\n",mysqli_connect_errno());
+    exit();
+}
+
+?>
